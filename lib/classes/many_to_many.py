238a8d6921157a1e0d0efa6cf06b84d6efd5d58c
BlessingOkora <moraablessing082@gmail.com>
2024-12-09 08:54:19 +0300

    corrects errors

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -3,51 +3,52 @@ class Article:
 
     def __init__(self, author, magazine, title):
         if not isinstance(author, Author):
-            raise ValueError("Author must be an instance of Author.")
+            raise ValueError("author must be an instance of Author")
         if not isinstance(magazine, Magazine):
-            raise ValueError("Magazine must be an instance of Magazine.")
-        if not isinstance(title, str) or len(title) == 0:
-            raise ValueError("Title must be a non-empty string.")
+            raise ValueError("magazine must be an instance of Magazine")
+        if not isinstance(title, str) or not (5 <= len(title) <= 50):
+            raise ValueError("title must be a string between 5 and 50 characters")
 
+        self._title = title
         self.author = author
         self.magazine = magazine
-        self.title = title
         Article.all.append(self)
+
+    @property
+    def title(self):
+        return self._title
+
 class Author:
     def __init__(self, name):
-        self.name = name
+        if not isinstance(name, str) or len(name) == 0:
+            raise ValueError("name must be a non-empty string")
+        self._name = name
 
     @property
     def name(self):
         return self._name
-    
-    @name.setter
-    def name(self, value):
-        if not isinstance(value, str) or len(value) == 0:
-            raise ValueError("Name must be a non-empty string.")
-        self._name = value
 
     def articles(self):
         return [article for article in Article.all if article.author == self]
 
     def magazines(self):
-        return list({article.magazine for article in self.articles()})
+        return list(set(article.magazine for article in self.articles()))
 
     def add_article(self, magazine, title):
         return Article(self, magazine, title)
 
     def topic_areas(self):
-        return list({magazine.category for magazine in self.magazines()})
-
+        return list(set(magazine.category for magazine in self.magazines()))
+ 
 class Magazine:
     def __init__(self, name, category):
         self.name = name
         self.category = category
-        
+
     @property
     def name(self):
         return self._name
-    
+
     @name.setter
     def name(self, value):
         if not isinstance(value, str) or not (2 <= len(value) <= 16):
@@ -57,21 +58,27 @@ class Magazine:
     @property
     def category(self):
         return self._category
-    
+
     @category.setter
     def category(self, value):
-        if not isinstance(value, str) or len(value) == 0:
+        if not isinstance(value, str) or len(value.strip()) == 0:
             raise ValueError("Category must be a non-empty string.")
         self._category = value
-    
+
     def articles(self):
-        pass
+        return [article for article in Article.all if article.magazine == self]
 
     def contributors(self):
         return list({article.author for article in Article.all if article.magazine == self})
 
     def article_titles(self):
-        return [article.title for article in Article.all if article.magazine == self]
+        return [article.title for article in self.articles()]
 
     def contributing_authors(self):
-        pass
+        author_article_count = {}
+        for article in Article.all:
+            if article.magazine == self:
+                if article.author not in author_article_count:
+                    author_article_count[article.author] = 0
+                author_article_count[article.author] += 1
+        return [author for author, count in author_article_count.items() if count > 2]
